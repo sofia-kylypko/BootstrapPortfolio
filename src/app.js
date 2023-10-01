7d9ce757f0554284cd8f902f1e8c7460a379db2a
Sofiia Kylypko <sofia.kilipko@gmail.com>
2023-10-01 13:30:51 +0100

    added js file, done home page

---
+++ src/app.js
@@ -0,0 +1,6 @@
+let btnWeather=document.getElementById("weatherBTN");
+
+btnWeather.addEventListener("click",()=> {
+    console.log(1);
+    window.location.href = 'https://comforting-mandazi-cf1db8.netlify.app/';
+});

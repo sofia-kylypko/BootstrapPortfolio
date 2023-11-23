d8d341535daef71bed39bd99fac384b4c2142c5d
Sofiia Kylypko <sofia.kilipko@gmail.com>
2023-11-23 17:52:49 +0000

    adding new project- react weather app, to projects stack

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 let btnWeather=document.getElementById("weatherBTN");
+let btnTransition=document.getElementById("transition");
 
 btnWeather.addEventListener("click",()=> {
     console.log(1);
     window.location.href = 'https://comforting-mandazi-cf1db8.netlify.app/';
+});
+
+btnTransition.addEventListener("click",()=> {
+    let link = btnTransition.getAttribute("props");
+    window.location.href = link;
 });
